9a87e39d08b26d03ac2753d5c408fa6c20ef46e8
Jan Kevin Gerona <120094704+blckzr@users.noreply.github.com>
2024-11-26 15:05:54 +0800

    Update a-star.py

--- a-star.py
+++ a-star.py
@@ -1,3 +1,7 @@
+# Reference:
+# https://medium.com/@nicholas.w.swift/easy-a-star-pathfinding-7e6689c7f7b2
+# https://www.geeksforgeeks.org/a-search-algorithm/
+
 class Cell():
     def __init__(self, parent=None, position=None):
         self.parent = parent
